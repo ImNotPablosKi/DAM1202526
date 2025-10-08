24b319a76461a23b146be0aa70f7d39064daf8b4
ImNotPablosKi <pmigueld@iesch.org>
2025-10-08 17:29:16 +0200

    Primeros Ejercicios prácticos JAVA

---
+++ PRO/JAVA/EjerciciosClase/ejercicio4.java
@@ -0,0 +1,15 @@
+package EjerciciosClase;
+
+public class ejercicio4 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c;
+
+        c = a;
+        a = b;
+        b = a;
+
+    }
+}
