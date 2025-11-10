6e28665c6f451e16fe90808323dcfedd66690dc8
ImNotPablosKi <pmigueld@iesch.org>
2025-11-10 20:18:58 +0100

    Add comment to probatinas

--- PRO/JAVA/TEMA3/Pruebas/random.java
+++ PRO/JAVA/TEMA3/Pruebas/random.java
@@ -12,7 +12,8 @@ public class random {
             int num = (int) (Math.random() * (100) + 1);
             System.out.println(num);
 
-            // La buena
+            // La buena, en este caso entre 50 y 100, el numero minimo a menos que sea 1 DEBE ser una variable.
+            // Si no se enfada, y bastante.
             int random = (int) (Math.random() * (max - min + 1) + min);
             System.out.println(random);
 
