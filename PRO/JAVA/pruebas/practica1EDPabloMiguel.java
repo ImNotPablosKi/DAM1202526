416d2f4a480bf426b10c1a14e9069da9a8fc3ead
ImNotPablosKi <pmigueld@iesch.org>
2025-11-14 21:02:01 +0100

    ED: Práctica 1 (FAILED)

---
+++ PRO/JAVA/pruebas/practica1EDPabloMiguel.java
@@ -0,0 +1,22 @@
+package pruebas;
+import java.util.Random;
+
+public class practica1EDPabloMiguel {
+    public static void main(String[] args) {
+        // Crear un objeto Random para generar números aleatorios
+        Random random = new Random();
+        // Obtener un número aleatorio entre 1 y 10 (ambos incluidos)
+        int iter = random.nextInt(5) + 1; // nextInt(20) genera un número entre 0 y 4
+        int count = random.nextInt(10) + 1; // nextInt(20) genera un número entre 0 y 9
+        System.out.println("Iteraciones: " + iter + " Números: " + count + ".Cantidad de números a generar: " + iter * count);
+        for (int i = 0; i < iter; i++)
+        {
+            System.out.println("Iteración número: " + i);
+            int j = 0;
+            while (j < count) {
+                System.out.println("-> Iter. " + j + ", valor de i: " + i);
+                j++;
+            }
+        }
+    }
+}
