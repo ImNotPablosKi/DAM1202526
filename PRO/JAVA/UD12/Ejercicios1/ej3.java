1c23af157512656c66a50a91d700bde1ddc9e66b
ImNotPablosKi <pmigueld@iesch.org>
2025-12-15 21:18:01 +0100

    Java: UD12 Finalizada, comiento UD8

---
+++ PRO/JAVA/UD12/Ejercicios1/ej3.java
@@ -0,0 +1,17 @@
+package UD12.Ejercicios1;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ej3 {
+    public static void main(String[] args) throws IOException {
+
+        Path ruta = Paths.get("hola");
+
+        Files.createDirectory(ruta);
+
+    }
+}
