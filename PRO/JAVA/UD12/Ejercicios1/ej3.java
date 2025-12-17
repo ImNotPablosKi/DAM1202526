0f1be81cd827d8ec912453af8323639779b582a4
ImNotPablosKi <pmigueld@iesch.org>
2025-12-17 17:34:00 +0100

    Java: Fin Bloque Ejercicios 1, Uso de BufferedReader Termino el bloque de Ejercicios 1 y empiezo el Bloque Nº2, Uso de BufferedReader ft.@hector6933 (Hecprooll)

--- PRO/JAVA/UD12/Ejercicios1/ej3.java
+++ PRO/JAVA/UD12/Ejercicios1/ej3.java
@@ -9,9 +9,56 @@ import java.nio.file.Paths;
 public class ej3 {
     public static void main(String[] args) throws IOException {
 
-        Path ruta = Paths.get("hola");
+        // Para ahorrarme escribir cosas uh
+        Path rutaGeneral = Paths.get("cosa/PRO_UD08_Ej03/");
 
-        Files.createDirectory(ruta);
+        Path ruta = Paths.get("cosa/PRO_UD08_Ej03/Programacion.txt");
+        Path ruta2 = Paths.get("cosa/PRO_UD08_Ej03/Programacion(+1).txt");
+        Path ruta3 = Paths.get("cosa/PRO_UD08_Ej03/Programacion(+2).txt");
+        Path ruta4 = Paths.get("cosa/PRO_UD08_Ej03/docs/samples");
+        Path ruta5 = Paths.get(rutaGeneral + "docs/exercises");
+
+        try {
+
+            Files.copy(ruta, ruta2);
+            Files.copy(ruta, ruta3);
+
+        } catch (IOException e) {
+
+            System.out.println("Error de Copia");
+
+        }
+
+        try {
+
+            Files.createDirectories(ruta4);
+            Files.createDirectory(ruta5);
+
+        } catch (IOException e) {
+
+            System.out.println("Error de Creación de carpetas");
+
+        }
+
+        try {
+
+            Files.move(ruta2, Paths.get("cosa/PRO_UD08_Ej03/Programacion.txt"));
+
+        } catch (IOException e) {
+
+            System.out.println("Error al renombrar ficheros.");
+
+        }
+
+        try {
+
+            Files.delete(ruta3);
+
+        } catch (IOException e) {
+
+            System.out.println("Error de borrado de archivos");
+
+        }
 
     }
 }
---
+++ PRO/JAVA/UD12/Ejercicios2/ej5.java
@@ -0,0 +1,41 @@
+package UD12.Ejercicios2;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ej5 {
+    public static void main(String[] args) {
+
+        File source = new File("cosa/EJ5/ej5.txt");
+        File target = new File("cosa/EJ5/target.txt");
+
+        if (!source.exists() || !target.exists()) {
+
+            System.out.println("El archivo solicitado NO existe!");
+
+        } else {
+
+            try (BufferedReader br = new BufferedReader(new FileReader(source)); BufferedWriter bw = new BufferedWriter((new FileWriter(target)))) {
+
+                System.out.println("Texto Original:");
+
+                String line;
+
+                while ((line = br.readLine()) != null) {
+
+                    System.out.println(line);
+                    bw.write(line + "\n");
+
+                }
+
+            } catch (Exception e) {
+
+                System.out.println("Se ha producido un error de lectura");
+
+            }
+
+        }
+
+    }
+}
