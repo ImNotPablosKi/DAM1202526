350ce674d8f7af355de5039c96620bc30ed822d6
ImNotPablosKi <pmigueld@iesch.org>
2026-01-15 18:22:52 +0100

    JAVA: Nueva tech para instanciar Objetos; Repaso de Herencia Y Polimorfismo

--- PRO/UD6OOP/src/HERENCIA/DemoVehiculos.java
+++ PRO/UD6OOP/src/HERENCIA/DemoVehiculos.java
@@ -8,21 +8,25 @@ public class DemoVehiculos {
 
         ArrayList<Vehiculo> misVehiculos = new ArrayList<>();
 
-        Coche c1 = new Coche("Mercedes", "SLG McLaren");
-        Motocicleta m1 = new Motocicleta("KTM", "KT-2");
-
-        misVehiculos.add(c1);
-        misVehiculos.add(m1);
+        misVehiculos.add(new Coche("Mercedes", "SLG McLaren"));
+        misVehiculos.add(new Motocicleta("KTM", "KT-2"));
+        misVehiculos.add(new Coche("Mercedes BENZ", "AMG GT3"));
 
         for (Vehiculo v: misVehiculos) {
 
             v.arrancar();
 
+            if (v instanceof Coche) {
+
+                ((Coche) v).abrirMaletero();
+
+            } else if (v instanceof Motocicleta) {
+
+                ((Motocicleta) v).hacerCaballito();
+
+            }
+
         }
-
-        c1.abrirMaletero();
-        m1.hacerCaballito();
-
     }
 
 }
