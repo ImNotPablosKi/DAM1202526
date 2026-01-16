495ddf8dcebc12c42d38b3a9a5ec4a9bbf7a3c85
ImNotPablosKi <pmigueld@iesch.org>
2026-01-16 17:30:43 +0100

    JAVA: EJ19, Terminar en casa o más tarde.

---
+++ PRO/JAVA/UD12/Ejercicios4/ej19.java
@@ -0,0 +1,15 @@
+package UD12.Ejercicios4;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ej19 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+//        Matcher passwd = Pattern.compile("\\b[([A-Z]{1,})&(\\d{1,}]\\b");
+
+    }
+}
