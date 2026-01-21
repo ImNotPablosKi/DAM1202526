ed271bdc50c0baaf141769101b237eddc861cd63
unknown <pmigueld@iesch.org>
2026-01-21 23:32:13 +0100

    JAVA: Bloque de Ejercicios 5 Terminado; BLOQUE UD10 Terminado

---
+++ PRO/JAVA/UD10/Ejercicios5/ej23.java
@@ -0,0 +1,27 @@
+package UD10.Ejercicios5;
+
+import java.io.*;
+
+public class ej23 {
+    public static void main(String[] args) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader("cosa/multiusos.txt")); BufferedWriter bw = new BufferedWriter(new FileWriter("cosa/target.txt"))) {
+
+            String line;
+
+            while ((line = br.readLine()) != null) {
+
+                bw.write(line.replaceAll("temperatura", "breva"));
+
+            }
+
+        } catch (IOException e) {
+
+            System.out.println("nuh uh");
+
+        }
+
+
+
+    }
+}
