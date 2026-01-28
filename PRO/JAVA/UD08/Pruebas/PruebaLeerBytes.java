0c41a1112847390e0276d5767eccca18c3d2a55f
ImNotPablosKi <pmigueld@iesch.org>
2026-01-28 17:32:26 +0100

    JAVA: Práctica Repaso UD10 Polimorfismo

--- PRO/JAVA/UD08/Pruebas/PruebaLeerBytes.java
+++ PRO/JAVA/UD08/Pruebas/PruebaLeerBytes.java
@@ -1,7 +1,6 @@
 package UD08.Pruebas;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -10,13 +9,14 @@ public class PruebaLeerBytes {
 
         Path ruta = Paths.get("cosa/multiusos.txt");
 
-        try (FileInputStream leer = new FileInputStream("cosa/multiusos.txt")) {
+        try (FileInputStream leer = new FileInputStream("cosa/multiusos.txt"); FileOutputStream escribir = new FileOutputStream("cosa/escribir.txt"))  {
 
             int data;
 
             while ((data = leer.read()) != -1) {
 
                 System.out.print((char) data);
+                escribir.write(data);
 
             }
 
