f4bcdad64a295b47e4411669108ca47870549b6b
ImNotPablosKi <pmigueld@iesch.org>
2026-02-03 20:12:29 +0100

    JAVA: RandomAccessFile

---
+++ PRO/JAVA/UD08/Pruebas/RandomAccessLeer.java
@@ -0,0 +1,43 @@
+package UD08.Pruebas;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class RandomAccessLeer {
+    public static void main(String[] args) {
+
+        try (RandomAccessFile raf = new RandomAccessFile("cosa/escribir.txt", "rw")) {
+
+            // raf.seek(8);
+
+            raf.write("Me cago en la puta".getBytes());
+            raf.write(" coñete".getBytes());
+            raf.write("\nJodete microslop".getBytes());
+
+            String line;
+
+            raf.seek(0);
+
+//            while ((line = raf.readLine()) != null) {
+//
+//                System.out.println(line);
+//
+//            }
+
+            int data;
+
+            while ((data = raf.read()) != -1 ) {
+
+                System.out.print((char) data);
+
+            }
+
+        } catch (IOException e) {
+
+            System.out.println("nuh uh");
+
+        }
+
+    }
+}
