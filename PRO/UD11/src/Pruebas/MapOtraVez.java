817b37d5cb89cb529ca005a457162211a5d9ec12
ImNotPablosKi <pmigueld@iesch.org>
2026-03-02 15:00:21 +0100

    we're so washed together twin

--- PRO/UD11/src/Pruebas/MapOtraVez.java
+++ PRO/UD11/src/Pruebas/MapOtraVez.java
@@ -1,27 +1,30 @@
 package Pruebas;
 
+import java.util.HashMap;
 import java.util.Scanner;
-import java.util.TreeMap;
 
-public class PruebaMap {
+public class MapOtraVez {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Dime algo así guapo anda: ");
+        System.out.println("Dime algo: ");
         String frase = sc.nextLine();
 
         frase = frase.replaceAll(" ", "");
 
-        TreeMap<Character, Integer> letras = new TreeMap<>();
+        // El HashMap no lo ordena alfabéticamente :C
+        HashMap<Character, Integer> letras = new HashMap<>();
 
-        for (int i = 0; i < frase.length(); i++) {
+        for (int i = 0; i < frase.length() ; i++) {
 
             letras.put(frase.charAt(i), letras.containsKey(frase.charAt(i)) ? letras.get(frase.charAt(i)) + 1 : 1);
 
         }
 
         System.out.println(letras);
+
+        sc.close();
 
     }
 }
