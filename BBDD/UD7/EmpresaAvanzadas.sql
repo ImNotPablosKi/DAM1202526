9bad524cf49633b1d5e5764f6b5c926b6b3938a0
ImNotPablosKi <pmigueld@iesch.org>
2026-03-09 17:31:59 +0100

    BBDD: Consultas

--- BBDD/UD7/EmpresaAvanzadas.sql
+++ BBDD/UD7/EmpresaAvanzadas.sql
@@ -47,3 +47,67 @@ SELECT NomDep, CONCAT(CodDep,' ', FLOOR(10 + (RAND() * 90)),' ',CodCen) AS Codig
 -- AGRUPACIONES
 -- ----------------
 
+-- 1
+SELECT NomEmp, COUNT(NumHi) FROM empleado WHERE NumHi = 2 GROUP BY NomEmp;
+
+-- 2
+SELECT COUNT(*) AS CuantosEmpleados FROM empleado WHERE ExTelEmp IS NULL;
+
+-- 3
+SELECT AVG(TIMESTAMPDIFF(YEAR, FecNaEmp, CURDATE())) AS 'Edad Media' FROM empleado;
+
+-- 4
+SELECT AVG(TIMESTAMPDIFF(YEAR, e.FecNaEmp, CURDATE())) AS 'Edad Media' FROM empleado e LEFT JOIN departamento d
+ON e.CodDep=d.CodDep WHERE e.CodEmp <> d.CodEmpDir;
+
+-- 5
+SELECT AVG(TIMESTAMPDIFF(YEAR, e.FecNaEmp, CURDATE())) AS 'Edad Media' FROM empleado e LEFT JOIN departamento d
+ON e.CodDep=d.CodDep LEFT JOIN centro c ON d.CodCen=c.CodCen WHERE c.PobCen LIKE 'Murcia';
+
+-- 6
+SELECT SUM(d.PreAnu) FROM departamento d LEFT JOIN centro c ON d.CodCen=c.CodCen WHERE c.PobCen LIKE 'Cartagena';
+
+-- 7
+SELECT COUNT(*) FROM empleado WHERE CodEmp IN (SELECT CodEmp FROM habemp);
+
+-- 8
+SELECT COUNT(*) FROM empleado WHERE CodEmp NOT IN (SELECT CodEmp FROM habemp);
+
+-- 9
+SELECT d.CodDep, d.NomDep, MIN(e.SalEmp) AS 'PresupuestoMinimo', MAX(e.SalEmp) AS 'PresupuestoMinimo', AVG(e.SalEmp) AS 'PresupuestoMedio' FROM departamento d
+LEFT JOIN empleado e ON d.CodDep=e.CodDep GROUP BY CodDep;
+
+-- 10
+SELECT e.CodEmp, e.NomEmp, COUNT(*) AS NumeroHabilidades FROM empleado e NATURAL JOIN habemp h GROUP BY CodEmp;
+
+-- 11
+SELECT d.CodDep, d.NomDep, COUNT(*) AS HabilidadesDeSusEmpleados FROM departamento d NATURAL JOIN empleado e
+NATURAL JOIN habemp h GROUP BY CodDep;
+
+-- 12
+SELECT c.CodCen, c.NomCen, COUNT(*) AS HabilidadesDeEmpleadosPorCentro FROM centro c
+LEFT JOIN departamento d ON c.CodCen=d.CodCen NATURAL LEFT JOIN empleado e NATURAL RIGHT JOIN habemp h GROUP BY c.CodCen;
+
+-- 13
+SELECT c.CodCen, c.NomCen, d.CodDep, d.NomDep, COUNT(*) AS HabilidadesDeEmpleadosPorCentro FROM centro c
+LEFT JOIN departamento d ON c.CodCen=d.CodCen NATURAL LEFT JOIN empleado e NATURAL RIGHT JOIN habemp h GROUP BY CodDep;
+
+-- 14
+SELECT c.CodCen, AVG(e.SalEmp) FROM centro c LEFT JOIN departamento d
+ON c.CodCen=d.CodCen NATURAL LEFT JOIN empleado e WHERE d.PreAnu > 25000000 GROUP BY CodCen ORDER BY AVG(e.SalEmp) DESC;
+
+-- 15
+SELECT c.PobCen, SUM(e.SalEmp) FROM centro c LEFT JOIN departamento d ON c.CodCen=d.CodCen
+NATURAL JOIN empleado e GROUP BY c.PobCen;
+
+-- 16
+SELECT  d.NomDep, (SELECT  COUNT(*) FROM  empleado e WHERE  e.CodDep=d.CodDep) FROM  departamento d 
+WHERE (SELECT COUNT(*) FROM  empleado e WHERE  e.CodDep=d.CodDep)>3;
+
+-- 17
+SELECT cen.NomCen, (SELECT COUNT(*) FROM empleado emp1 WHERE dep.CodDep=emp1.CodDep) AS 'Numero empleados'
+FROM centro cen LEFT JOIN departamento dep ON cen.CodCen=dep.CodCen
+WHERE (SELECT COUNT(*) FROM empleado emp2 WHERE dep.CodDep=emp2.CodDep) > 3;
+
+
+
