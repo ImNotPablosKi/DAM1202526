5e91f95c6c050c4899a464df21b291a0c147fb59
ImNotPablosKi <pmigueld@iesch.org>
2026-05-15 19:35:29 +0200

    asdasd2

--- BBDD/PABLO_SQL/UD10/ejemploCursores.sql
+++ BBDD/PABLO_SQL/UD10/ejemploCursores.sql
@@ -32,3 +32,56 @@ BEGIN
 	CLOSE cursor_ciudades;
 	
 END//
+
+DROP PROCEDURE IF EXISTS world.resumen_idiomas_continente;//
+CREATE PROCEDURE world.resumen_idiomas_continente()
+BEGIN
+	
+	DECLARE continente VARCHAR(25);
+	DECLARE cant_idiomas INT;
+	DECLARE porcentaje DECIMAL(5,2);
+	
+	-- Declarar variable ultima_fila del cursor
+	DECLARE ultima_fila BOOLEAN DEFAULT FALSE;
+	
+	-- Declarar el cursor
+	DECLARE cursor_continente CURSOR FOR
+		SELECT DISTINCT Continent FROM country;
+	
+	-- Declarar el manejador de errores para el cursor
+	DECLARE CONTINUE HANDLER FOR NOT FOUND
+		SET ultima_fila = TRUE;
+	
+	CREATE TEMPORARY TABLE temporal_continente(
+		continente VARCHAR(25),
+		num_idiomas INT,
+		porcentaje DECIMAL(5,2)
+	);
+	
+	-- Abrir el cursor
+	OPEN cursor_continente;
+	
+	-- Procesar, Usamos un bucle para recorrer el cursor
+	bucle_cursor: LOOP
+		-- Procesar un cursor
+		FETCH cursor_continente INTO continente;
+	
+		-- Condicion de salida del bucle
+		IF ultima_fila THEN
+			LEAVE bucle_cursor;
+		END IF;
+		
+		SELECT COUNT(cl.Language), AVG(cl.percentage)
+		INTO cant_idiomas, porcentaje
+		FROM country c JOIN countrylanguage cl
+		ON c.Code = cl.CountryCode
+		WHERE c.Continent = continent;
+		
+		INSERT INTO temporal_continentes VALUES(continente, num_idiomas, porcentaje);
+		
+	END LOOP;
+	
+	CLOSE cursor_continente;
+	
+	SELECT * FROM temporal_continente;
+END//
